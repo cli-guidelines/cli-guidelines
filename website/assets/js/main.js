372f8ccb1fd0cc0e0804749a8e3ce77cb777bcf3
Aanand Prasad <aanand.prasad@gmail.com>
2020-10-25 13:15:08 -0400

    Convert page to nested sections, then spy on those

--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -1,75 +1,67 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    const navLinks = document.querySelectorAll('nav li a');
-    const elements = document.querySelectorAll('main > *');
-    startNavObservation(elements, navLinks);
+    convertToNestedSections(document.querySelector('main'));
+    startNavObservation();
 
 });
 
-function startNavObservation(elements, navLinks) {
-    const headingMap = buildHeadingMap(elements);
+function convertToNestedSections(rootElement) {
+    const children = Array.from(rootElement.children);
 
-    const visibleElements = new Set();
+    children.forEach(element => rootElement.removeChild(element));
+
+    let currentSection = rootElement;
+    let currentLevel = 0;
+
+    children.forEach(element => {
+        const headingMatch = element.tagName.match(/^h(\d)$/i);
+
+        if (headingMatch) {
+            const newLevel = parseInt(headingMatch[1]);
+
+            while (currentLevel + 1 < newLevel) {
+                const section = document.createElement('section');
+                currentSection.appendChild(section);
+                currentSection = section;
+                currentLevel++;
+            }
+
+            while (currentLevel + 1 > newLevel) {
+                currentSection = currentSection.parentNode;
+                currentLevel--;
+            }
+
+            const newSection = document.createElement('section');
+            newSection.setAttribute('id', element.getAttribute('id'));
+            element.removeAttribute('id');
+
+            currentSection.appendChild(newSection);
+
+            currentSection = newSection;
+            currentLevel = newLevel;
+        }
+
+        currentSection.appendChild(element);
+    });
+}
+
+// https://www.bram.us/2020/01/10/smooth-scrolling-sticky-scrollspy-navigation/
+function startNavObservation() {
 
     const observer = new IntersectionObserver(entries => {
-        entries.forEach(e => {
-            if (e.isIntersecting) {
-                visibleElements.add(e.target);
-            } else {
-                visibleElements.delete(e.target);
-            }
-        });
+		entries.forEach(entry => {
+			const id = entry.target.getAttribute('id');
+			if (entry.intersectionRatio > 0) {
+				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
+			} else {
+				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
+			}
+		});
+	});
 
-        const topmostElement = getTopmostElement(visibleElements);
-        if (!topmostElement) {
-            return;
-        }
-
-        const closestHeading = headingMap.get(topmostElement);
-
-        updateNavigation(navLinks, closestHeading);
+	// Track all sections that have an `id` applied
+	document.querySelectorAll('section[id]').forEach((section) => {
+		observer.observe(section);
     });
 
-    elements.forEach(e => observer.observe(e));
-}
-
-function buildHeadingMap(elements) {
-    const map = new Map();
-    let lastHeading = null;
-
-    elements.forEach(element => {
-        if (element.matches('h2,h3,h4,h5,h6') && element.hasAttribute('id')) {
-            lastHeading = element;
-        }
-
-        map.set(element, lastHeading);
-    });
-
-    return map;
-}
-
-function getTopmostElement(elements) {
-    const elementsArray = Array.from(elements);
-
-    elementsArray.sort((a, b) => {
-        if (a === b) {
-            return 0;
-        }
-        if (a.compareDocumentPosition(b) & Node.DOCUMENT_POSITION_PRECEDING) {
-            return 1;
-        }
-        return -1;
-    });
-
-    return elementsArray[0];
-}
-
-function updateNavigation(navLinks, currentHeading) {
-    navLinks.forEach(a => {
-        if (currentHeading && a.getAttribute('href') === `#${currentHeading.id}`) {
-            a.parentNode.classList.add('active');
-        } else {
-            a.parentNode.classList.remove('active');
-        }
-    });
 }
