eeab9404def8d79f332f1bd42af2a2f420a1dea1
Aanand Prasad <aanand.prasad@gmail.com>
2020-10-25 13:33:01 -0400

    Don't crash if a matching link isn't found

--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -49,19 +49,22 @@ function convertToNestedSections(rootElement) {
 function startNavObservation() {
 
     const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			const id = entry.target.getAttribute('id');
-			if (entry.intersectionRatio > 0) {
-				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
-			} else {
-				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
-			}
-		});
-	});
+        entries.forEach(entry => {
+            const id = entry.target.getAttribute('id');
+            const link = document.querySelector(`nav li a[href="#${id}"]`);
+            if (link) {
+                if (entry.intersectionRatio > 0) {
+                    link.parentElement.classList.add('active');
+                } else {
+                    link.parentElement.classList.remove('active');
+                }
+            }
+        });
+    });
 
-	// Track all sections that have an `id` applied
-	document.querySelectorAll('section[id]').forEach((section) => {
-		observer.observe(section);
+    // Track all sections that have an `id` applied
+    document.querySelectorAll('section[id]').forEach((section) => {
+        observer.observe(section);
     });
 
 }
